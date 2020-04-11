00395d3fbb8c3f74c0be144b5c7b058786fb3c05
Peter van Dijk <peter.van.dijk@bynder.com>
2020-04-11 23:30:44 +0200

    with logger

--- src/lib/hooks/gtsportStore.ts
+++ src/lib/hooks/gtsportStore.ts
@@ -1,6 +1,7 @@
 import create, { StateCreator, SetState, GetState, StoreApi, PartialState } from 'zustand';
 import produce from 'immer';
-import { devtools } from 'zustand/middleware';
+// import { devtools } from 'zustand/middleware';
+import { mountStoreDevtool } from 'simple-zustand-devtools';
 import { UserInfo, JStatsDetails, IStatsHistory } from '../../types';
 
 /*
@@ -37,28 +38,32 @@ const log = (config: StateCreator<GTState>) => (
     api,
   );
 
-export const [gtsportStore] = create<GTState>(
-  // log(
-  (set): GTState => {
-    console.log('  init state');
-    return {
-      userId: '',
-      setUserId: (input: string) => set(state => ({ userId: input })),
-      userInfo: {} as UserInfo,
-      setUserInfo: (input: UserInfo) =>
-        set(state => ({
-          userInfo: input,
-        })),
-      statsDetails: {} as JStatsDetails,
-      setStatsDetails: (input: JStatsDetails) => set(state => ({ statsDetails: input })),
-      statsHistory: {} as IStatsHistory,
-      setStatsHistory: (input: IStatsHistory) => set(state => ({ statsHistory: input })),
-      triggerRequest: false,
-      setTriggerRequest: (input: boolean) => set(state => ({ triggerRequest: input })),
-    };
-  },
-  // ),
+export const [gtsportStore, store] = create<GTState>(
+  log(
+    (set): GTState => {
+      console.log('  init state');
+      return {
+        userId: '',
+        setUserId: (input: string) => set(state => ({ userId: input })),
+        userInfo: {} as UserInfo,
+        setUserInfo: (input: UserInfo) =>
+          set(state => ({
+            userInfo: input,
+          })),
+        statsDetails: {} as JStatsDetails,
+        setStatsDetails: (input: JStatsDetails) => set(state => ({ statsDetails: input })),
+        statsHistory: {} as IStatsHistory,
+        setStatsHistory: (input: IStatsHistory) => set(state => ({ statsHistory: input })),
+        triggerRequest: false,
+        setTriggerRequest: (input: boolean) => set(state => ({ triggerRequest: input })),
+      };
+    },
+  ),
 );
+
+if (process.env.NODE_ENV !== 'production') {
+  mountStoreDevtool('Store', store);
+}
 
 // Turn the set method into an immer proxy
 const immer = (config: StateCreator<GTState>) => (

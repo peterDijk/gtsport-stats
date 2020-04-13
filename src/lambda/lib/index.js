7d29c3e0517b9040b7b37e50e1f70b4433fecb8f
Peter van Dijk <peter.van.dijk@bynder.com>
2020-04-13 20:10:21 +0200

    test hardcoded cookie

--- src/lambda/lib/index.js
+++ src/lambda/lib/index.js
@@ -8,6 +8,8 @@ export async function serverSideRequest(url, params, responseCallback) {
     headers: {
       'Content-Type': 'application/x-www-form-urlencoded',
       'Access-Control-Allow-Credentials': true,
+      cookie:
+        '__cfduid=d48e78e5652d74784f3febbdab6e4604c1586798090; PHPSESSID=kh8sn6493cacushkn97dmji412; __utmc=137753130; __utmz=137753130.1586798089.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); __utma=137753130.799065148.1586798089.1586798089.1586801369.2; __utmt=1; __utmb=137753130.1.10.1586801369; __atuvc=2%7C16; __atuvs=5e94aad960611dc5000',
     },
   };
 
